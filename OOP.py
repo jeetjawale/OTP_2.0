f5fd5a20d6ed2b9b8933d1b6061c7e709a7173c2
Jeet Jawale <jeetjawale03@gmail.com>
2023-12-06 12:27:57 +0530

    oop version 3

--- OOP.py
+++ OOP.py
@@ -4,6 +4,7 @@ from twilio.rest import Client
 from dotenv import load_dotenv
 import os
 import re
+
 # Regular expression pattern for email validation
 email_pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
 
@@ -30,25 +31,25 @@ Please use this OTP to verify your identity. This code is valid for the next 5 m
             msg = f'Subject: {subject}\n\n{message}'
             server.sendmail(self.smtp_username, email, msg)
             server.quit()
-            print('OTP sent successfully via email!')
+            return True
         except Exception as e:
-            print(f'Error sending OTP via email: {str(e)}')
+            return f'Error sending OTP via email: {str(e)}'
 
 class MobileManager:
-    def __init__(self, twilio_account_sid, twilio_auth_token):
+    def __init__(self, twilio_account_sid, twilio_auth_token, twilio_phone_number):
         self.twilio_account_sid = twilio_account_sid
         self.twilio_auth_token = twilio_auth_token
+        self.twilio_phone_number = twilio_phone_number
 
     def send_otp(self, phone_number, otp):
         if self.twilio_account_sid is None or self.twilio_auth_token is None:
-            print("Twilio credentials not found.")
-            return
+            return "Twilio credentials not found."
 
         try:
             client = Client(self.twilio_account_sid, self.twilio_auth_token)
             message = client.messages.create(
                 to=phone_number,
-                from_='+15418358453',  # Replace with your Twilio phone number
+                from_=self.twilio_phone_number,
                 body=f'''Thank you for using. To enhance the security of your account, we have generated a one-time password (OTP) for you:
 
 Your OTP: {otp}
@@ -56,23 +57,14 @@ Your OTP: {otp}
 Please use this OTP to verify your identity. This code is valid for the next 5 minutes, after which it will expire.
 '''
             )
-            print('OTP sent successfully via Twilio!')
+            return True
         except Exception as e:
-            print(f'Error sending OTP via Twilio: {str(e)}')
+            return f'Error sending OTP via Twilio: {str(e)}'
 
 class OTPManager:
-    def __init__(self):
-        load_dotenv('.env')
-        self.email_manager = EmailManager(
-            smtp_server='smtp.gmail.com',
-            smtp_port=587,
-            smtp_username='jyjawale2003@gmail.com',
-            smtp_password=os.getenv('SMTP_PASSWORD')
-        )
-        self.mobile_manager = MobileManager(
-            twilio_account_sid=os.getenv('TWILIO_ACCOUNT_SID'),
-            twilio_auth_token=os.getenv('TWILIO_AUTH_TOKEN')
-        )
+    def __init__(self, email_manager, mobile_manager):
+        self.email_manager = email_manager
+        self.mobile_manager = mobile_manager
 
     def generate_otp(self):
         return str(random.randint(100000, 999999))
@@ -85,28 +77,43 @@ class OTPManager:
 
     def send_otp_email(self, email, otp):
         if self.validate_email(email):
-            self.email_manager.send_otp(email, otp)
+            return self.email_manager.send_otp(email, otp)
         else:
-            print('Invalid email address!')
+            return 'Invalid email address!'
 
     def send_otp_mobile(self, mobile, otp):
         if self.validate_mobile(mobile):
-            self.mobile_manager.send_otp('+91' + mobile, otp)
+            return self.mobile_manager.send_otp('+91' + mobile, otp)
         else:
-            print('Invalid mobile number!')
+            return 'Invalid mobile number!'
 
 if __name__ == "__main__":
-    otp_manager = OTPManager()
+    load_dotenv('.env')
+
+    email_manager = EmailManager(
+        smtp_server='smtp.gmail.com',
+        smtp_port=587,
+        smtp_username='jyjawale2003@gmail.com',
+        smtp_password=os.getenv('SMTP_PASSWORD')
+    )
+
+    mobile_manager = MobileManager(
+        twilio_account_sid=os.getenv('TWILIO_ACCOUNT_SID'),
+        twilio_auth_token=os.getenv('TWILIO_AUTH_TOKEN'),
+        twilio_phone_number='+15418358453'
+    )
+
+    otp_manager = OTPManager(email_manager, mobile_manager)
 
     # User input for email
     email = input('Enter your email address: ')
     otp = otp_manager.generate_otp()
-    otp_manager.send_otp_email(email, otp)
+    result = otp_manager.send_otp_email(email, otp)
+    print(result)
 
     # User input for mobile number (Twilio)
     use_twilio = input('Do you want to send OTP via Twilio? (yes/no): ')
     if use_twilio.lower() == 'yes':
         mobile = input('Enter your mobile number: ')
-        otp_manager.send_otp_mobile(mobile, otp)
-
-
+        result = otp_manager.send_otp_mobile(mobile, otp)
+        print(result)
